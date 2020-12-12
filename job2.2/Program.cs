2841a59da849140a5c62227e26408a53d58703ce
Семен <72011937+Semen39rus@users.noreply.github.com>
2020-12-12 14:55:21 +0300

    Add files via upload

---
+++ job2.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace job2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b, c;
+                Console.WriteLine("Введите 1 значение");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите 2 значение");
+                b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите 3 значение");
+                c = int.Parse(Console.ReadLine());
+                int max = a;
+                if (b > max) max = b;
+                if (c > max) max = c;
+                Console.WriteLine("max=" + max);
+                if (a > b) a = b;
+                if (a > c) a = c;
+                Console.WriteLine("Min = {0}", a);
+            }
+            catch (Exception e)
+            {
+                Console.ReadKey();
+            }
+        }
+            
+    }
+}
